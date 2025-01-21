0c999fb73b3442b4e60cca87119d6b6ef19d9c92
Nicolas Bernard <nicolasemanuelbernard@gmail.com>
2025-01-21 13:19:59 -0500

    flux return model version

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ replicate.Client(api_token=api_token)
 
 class ImageRequest(BaseModel):
     prompt : str
+    model : str
 
 # Funcion que crea un nuevo modelo entrenado
 def create_model(model_name : str, trigger_word : str, file_path: str):
@@ -40,29 +41,33 @@ def create_model(model_name : str, trigger_word : str, file_path: str):
             destination=f"{model.owner}/{model.name}"
         )
 
+    return training
+
 # Función para ejecutar replicate.run y la creacion de imagen
 def create_trained_image(prompt: str, model : str)-> str:
-    
-    output = replicate.run(
-    f"travelinglos/{model}:3474c31a696bc0d0fbe034d544f0c43985c180a34e256f7a6e01136fe7b831ef",
-        input={
-            "model": "dev",
-            "prompt": f"{prompt}",
-            "go_fast": False,
-            "lora_scale": 1,
-            "megapixels": "1",
-            "num_outputs": 1,
-            "aspect_ratio": "1:1",
-            "output_format": "png",
-            "guidance_scale": 3,
-            "output_quality": 100,
-            "prompt_strength": 0.8,
-            "extra_lora" : "https://replicate.delivery/xezq/2zFIXstgEdpzB9pHuRj5tJKqrecs1U0lJUZOeNUVWVw8C55TA/trained_model.tar",
-            "extra_lora_scale": 1,
-            "num_inference_steps": 28
-        }
-    )
-    return str(output[0])
+    try:
+        output = replicate.run(
+            f"travelinglos/{model}",
+            input={
+                "model": "dev",
+                "prompt": f"{prompt}",
+                "go_fast": False,
+                "lora_scale": 1,
+                "megapixels": "1",
+                "num_outputs": 1,
+                "aspect_ratio": "1:1",
+                "output_format": "png",
+                "guidance_scale": 3,
+                "output_quality": 100,
+                "prompt_strength": 0.8,
+                "extra_lora": "https://replicate.delivery/xezq/2zFIXstgEdpzB9pHuRj5tJKqrecs1U0lJUZOeNUVWVw8C55TA/trained_model.tar",
+                "extra_lora_scale": 1,
+                "num_inference_steps": 28
+            }
+        )
+        return str(output[0])
+    except Exception as e:
+        raise RuntimeError(f"Error generating image: {e}")
 
 @app.post("/create_model")
 async def new_model(model_name: str, trigger_word: str, file: UploadFile = File(...)):
@@ -73,7 +78,7 @@ async def new_model(model_name: str, trigger_word: str, file: UploadFile = File(
             shutil.copyfileobj(file.file, buffer)
 
         # Llama a la función `create_model` y pasa el archivo .zip temporal
-        create_model(model_name, trigger_word, file_path)
+        training = create_model(model_name, trigger_word, file_path)
 
         # Elimina el archivo temporal
         os.remove(file_path)
@@ -84,6 +89,7 @@ async def new_model(model_name: str, trigger_word: str, file: UploadFile = File(
             "message": "Model created and training started successfully.",
             'model_name' : model_name,
             'trigger_word' : trigger_word,
+            'flux_model' : f"{model_name}:{training.version}"
         }
     except Exception as e:
         # Maneja errores y devuelve un código HTTP 400 (Bad Request)
@@ -98,4 +104,4 @@ async def new_model(model_name: str, trigger_word: str, file: UploadFile = File(
 @app.post("/create_image")
 async def create_image(request: ImageRequest):
     
-    return create_trained_image(prompt=request)
+    return create_trained_image(prompt=request.prompt, model=request.model)
